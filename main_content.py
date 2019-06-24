280d2fbad8bef8c800723bbb28164168acad3551
Annie Sutton <asutton@twitter.com>
2019-06-24 18:10:51 +0100

    adding gitginore file and env variables

--- main_content.py
+++ main_content.py
@@ -6,7 +6,7 @@ app = Flask("MyApp")
 port = int(os.environ.get("PORT", 5000))
 
 @app.route("/", methods=['GET', 'POST'])
-@app.route("/home", methods=['GET', 'POST'])
+#@app.route("/home", methods=['GET', 'POST']) - this is to call thr link /home at the end like you have evening below
 def home():
     return render_template('daytime.html', when="daytime")
 
@@ -17,28 +17,27 @@ def evening():
 @app.route("/success", methods=['GET','POST'])
 def success():
     formName=request.form['name']
-    formGuests=request.form['guests']
     formEmail=request.form['email']
     formVegetarian=request.form['foodpreference']
     formWhen=request.form['when']
-    send_simple_message(formName, formGuests, formEmail, formVegetarian, formWhen)
+    send_simple_message(formName, formEmail, formVegetarian, formWhen)
     return render_template('success.html', name=formName)
 
 import requests
 
-API_KEY = "cc1be912ad91e420f327101bb5d096b5-7bce17e5-c201b125"
-DOMAIN_NAME ="sandbox58c29f7f76574b62a838d23116825fa6.mailgun.org"
+API_KEY = os.environ.get('API_KEY', None)
+DOMAIN_NAME = os.environ.get('DOMAIN_NAME', None)
 
-def send_simple_message(name, guests, email, foodpreference, when):
+def send_simple_message(name, email, foodpreference, when):
     return requests.post(
         "https://api.mailgun.net/v3/"+DOMAIN_NAME+"/messages",
         auth=("api", API_KEY),
         data={"from": "Wedding planner <mailgun@"+DOMAIN_NAME+">",
               "to": ["supergirlsutton@gmail.com"],
               "subject": "New guests sign up",
-              "text": name + " " + " Guests: " + " " + str(guests) + " " + " Email: " + " " + str(email) + " " + " Food Preference: "
- 				+ str(foodpreference) + " When: " + str(when)
+              "text": name + " " +  " " + " Email: " + " " + str(email) + " " + " Food Preference: "
+ 				+ str(foodpreference) + " When:  " + str(when)
  		})
-if __name__ == "__main__":
 
-    app.run(host='0.0.0.0', port=port, debug=False)
+#this below line allows you to make changes while it updates the local server - screen shot on desktop - CHANGE TO FALSE on deploy
+app.run(host='0.0.0.0', port=port, debug=False)
